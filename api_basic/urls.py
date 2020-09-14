4f702678c7e241284059ff80d0ca9fda17547162
AnkitVats21 <akmmfkh536@gmail.com>
2020-09-14 23:38:31 +0530

    views created and project is running now

---
+++ api_basic/urls.py
@@ -0,0 +1,9 @@
+# api_basic/urls.py
+from django.urls import path
+from rest_framework.urlpatterns import format_suffix_patterns
+from api_basic import views
+urlpatterns = [
+    path('basic/', views.API_objects.as_view()),
+    path('basic/<int:pk>/', views.API_objects_details.as_view()),
+]
+urlpatterns = format_suffix_patterns(urlpatterns)
